a976461fe7f11afc811432d5f34afae3a9dd2452
Luisrosario2604 <luis.rosario2604@gmail.com>
2022-04-26 12:55:02 +0200

    [ADD] adding all

--- filter_class.py
+++ filter_class.py
@@ -3,148 +3,162 @@
 # Importing python3 from local, just use "python3 <binary>" if is not the same location
 
 # Imports
-import sys
-import argparse
-import os
-import cv2
 import numpy as np
 import random
-
 from particle_class import Particle
 
 # Global values
+perturbation = 20
 
 
 # Function declarations
 class Filter:
 
-	def __init__(self, particle_nb, particle_size, img_width, img_height):
-		if img_width // 3 <= particle_size[0]:
-			raise Exception("\033[1m" + "[ERROR] -> Particle size width is way to big" + "\033[0m")
+    def __init__(self, particle_nb, particle_size, img_width, img_height):
+        if img_width // 3 <= particle_size[0]:
+            raise Exception("\033[1m" + "[ERROR] -> Particle size width is way to big" + "\033[0m")
 
-		if img_height // 3 <= particle_size[1]:
-			raise Exception("\033[1m" + "[ERROR] -> Particle size height is way to big" + "\033[0m")
+        if img_height // 3 <= particle_size[1]:
+            raise Exception("\033[1m" + "[ERROR] -> Particle size height is way to big" + "\033[0m")
 
-		self.__particle_nb = particle_nb
-		self.__particle_size = particle_size
-		self.__img_width = img_width
-		self.__img_height = img_height
-		self.__particles = []
-		self.__particles_perturbation = []
-		self.__total_score = 0
-		self.__last_chosen_particle = None
+        self.__particle_nb = particle_nb
+        self.__particle_size = particle_size
+        self.__img_width = img_width
+        self.__img_height = img_height
+        self.__particles = []
+        self.__particles_perturbation = []
+        self.__particles_prediction = []
+        self.__total_score = 0
+        self.__chosen_particles = []
 
-	# Randomly spawns particles in the image
-	def intialise_random(self):
-		if self.__last_chosen_particle and self.__particles_perturbation != []:
-			self.__particles = self.__particles_perturbation
-		else:
-			self.__particles = []
-			for i in range(self.__particle_nb):
-				a = b = 0
-				a2 = self.__img_width - self.__particle_size[0]
-				b2 = self.__img_height - self.__particle_size[1]
+    # Randomly spawns particles in the image
+    def intialise_random(self):
+        if self.__particles_perturbation != []:
+            self.__particles = self.__particles_perturbation
+        else:
+            self.__particles = []
+            for i in range(self.__particle_nb):
+                a = b = 0
+                a2 = self.__img_width - self.__particle_size[0]
+                b2 = self.__img_height - self.__particle_size[1]
 
-				x_part = random.randint(a, a2)
-				y_part = random.randint(b, b2)
+                x_part = random.randint(a, a2)
+                y_part = random.randint(b, b2)
 
-				particle = Particle(x_part, y_part, self.__particle_size[0], self.__particle_size[1], i)
-				self.__particles.append(particle)
+                particle = Particle(x_part, y_part, self.__particle_size[0], self.__particle_size[1], i)
+                self.__particles.append(particle)
 
-	def perturbation(self, perturbation_differential):
-		self.__particles_perturbation = []
+    def perturbation(self, perturbation_differential):
+        self.__particles_perturbation = []
 
-		if self.__total_score > 0:
-			ids = []
-			scores = []
-			x_max = 0
-			y_max = 0
-			x_min = 10000
-			y_min = 10000
-			nb_particle_positiv = 0
+        if self.__total_score > 0:
+            ids = []
+            scores = []
+            x_max = 0
+            y_max = 0
+            x_min = 10000
+            y_min = 10000
+            nb_particle_positiv = 0
 
-			for particle in self.__particles:
+            for particle in self.__particles:
 
-				ids.append(particle.get_id())
-				scores.append(particle.get_score())
-				print(particle.get_score())
+                ids.append(particle.get_id())
+                scores.append(particle.get_score())
 
-				# Getting x, y in min and max position for gaussian perturbation
-				if particle.get_score() > 0:
-					x, y, w, h = particle.get_coords()
-					x_max = max(x, x_max)
-					y_max = max(y, y_max)
-					x_min = min(x, x_min)
-					y_min = min(y, y_min)
-					nb_particle_positiv += 1
+                # Getting x, y in min and max position for gaussian perturbation
+                if particle.get_score() > 0:
+                    x, y, w, h = particle.get_coords()
+                    x_max = max(x, x_max)
+                    y_max = max(y, y_max)
+                    x_min = min(x, x_min)
+                    y_min = min(y, y_min)
+                    nb_particle_positiv += 1
 
-			# Doing the roulette method
-			probs = np.array(scores, dtype=int) / int(sum(scores))
-			sample_np = np.random.choice(ids, self.__particle_nb, p=probs)
+            # Doing the roulette method
+            probs = np.array(scores, dtype=int) / int(sum(scores))
+            sample_np = np.random.choice(ids, self.__particle_nb, p=probs)
 
-			# Creating a particule for each perturbation
-			for i, sample in enumerate(sample_np):
-				x, y, w, h = self.__particles[sample].get_coords()
+            # Creating a particule for each perturbation
+            for i, sample in enumerate(sample_np):
+                x, y, w, h = self.__particles[sample].get_coords()
 
-				print("X " + str(x_max + self.__particle_size[0] - x_min) + "  max" + str(x_max) + "  min" + str(x_min))
-				print("Y " + str(y_max + self.__particle_size[1] - y_min) + "  max" + str(y_max) + "  min" + str(y_min))
+                if perturbation_differential is not True:
+                    x = int(np.random.normal(x, perturbation))
+                    y = int(np.random.normal(y, perturbation))
+                elif nb_particle_positiv <= 1:
+                    x = int(np.random.normal(x, (x_max + self.__particle_size[0] - x_min) // 2))
+                    y = int(np.random.normal(y, (y_max + self.__particle_size[1] - y_min) // 2))
+                else:
+                    x = int(np.random.normal(x, (x_max - x_min) // 2))
+                    y = int(np.random.normal(y, (y_max - y_min) // 2))
 
-				if perturbation_differential is not True:
-					x = int(np.random.normal(x, 20))
-					y = int(np.random.normal(y, 20))
-				elif nb_particle_positiv <= 1:
-					x = int(np.random.normal(x, (x_max + self.__particle_size[0] - x_min) // 2))
-					y = int(np.random.normal(y, (y_max + self.__particle_size[1] - y_min) // 2))
-				else:
-					x = int(np.random.normal(x, (x_max - x_min) // 2))
-					y = int(np.random.normal(y, (y_max - y_min) // 2))
+                # Checking that no one of the perturbation is out of range than the image
+                # If yes the particle is replaced at max possible range in image
+                x = max(0, x)
+                y = max(0, y)
+                x = min(x, self.__img_width - self.__particle_size[0])
+                y = min(y, self.__img_height - self.__particle_size[1])
 
-				# Checking that no one of the perturbation is out of range than the image
-				# If yes the particle is replaced at max possible range in image
-				x = max(0, x)
-				y = max(0, y)
-				x = min(x, self.__img_width - self.__particle_size[0])
-				y = min(y, self.__img_height - self.__particle_size[1])
+                particle = Particle(x, y, w, h, i)
+                self.__particles_perturbation.append(particle)
 
-				particle = Particle(x, y, w, h, i)
-				self.__particles_perturbation.append(particle)
+    def calculate_score(self, mask):
+        nb_white_pxls = np.sum(mask == 255)
+        score = 0
+        total_score = 0
 
-	def calculate_score(self, mask):
-		nb_white_pxls = np.sum(mask == 255)
-		score = 0
-		total_score = 0
+        # Calculating the score for each particle (white pixels)
+        for particle in self.__particles:
+            x, y, w, h = particle.get_coords()
 
-		# Calculating the score for each particle (white pixels)
-		for particle in self.__particles:
-			x, y, w, h = particle.get_coords()
+            if nb_white_pxls > 0:
+                crop = mask[y:y+h, x:x+w]
+                score = np.sum(crop == 255)
+                total_score += score
 
-			if nb_white_pxls > 0:
-				crop = mask[y:y+h, x:x+w]
-				score = np.sum(crop == 255)
-				total_score += score
+            particle.set_score(score)
 
-			particle.set_score(score)
+        self.__total_score = total_score
 
-		self.__total_score = total_score
+    # Choose the particle with most score
+    def choose_particle(self):
+        if self.__total_score <= 0:
+            return
+        id = 0
+        score = 0
+        for particle in self.__particles:
+            if particle.get_score() >= score:
+                score = particle.get_score()
+                id = particle.get_id()
 
-	# Choose the particle with most score
-	def choose_particle(self):
-		if self.__total_score > 0:
-			id = 0
-			score = 0
-			for particle in self.__particles:
-				if particle.get_score() >= score:
-					score = particle.get_score()
-					id = particle.get_id()
+        self.__particles[id].set_chosen(True)
+        self.__chosen_particles.append(self.__particles[id])
 
-			self.__particles[id].set_chosen(True)
-			self.__last_chosen_particle = self.__particles[id]
+    def prediction(self):
+        if len(self.__chosen_particles) < 2:
+            return
+        self.__particles_prediction = []
 
-	def get_particles(self):
-		return self.__particles
+        x, y, w, h = self.__chosen_particles[-1].get_coords()
+        x2, y2, w2, h2 = self.__chosen_particles[-2].get_coords()
 
-	def get_last_chosen_particle(self):
-		return self.__last_chosen_particle
+        dir_x = x + (x - x2)
+        dir_y = y + (y - y2)
 
-	def get_particles_perturbation(self):
-		return self.__particles_perturbation
+        for i in range(self.__particle_nb):
+            pert_y = int(np.random.normal(dir_y, perturbation))
+            pert_x = int(np.random.normal(dir_x, perturbation))
+            particle = Particle(pert_x, pert_y, w, h, i)
+            self.__particles_prediction.append(particle)
+
+    def get_particles(self):
+        return self.__particles
+
+    def get_chosen_particles(self):
+        return self.__chosen_particles
+
+    def get_particles_perturbation(self):
+        return self.__particles_perturbation
+
+    def get_particles_prediction(self):
+        return self.__particles_prediction
--- main_filter.py
+++ main_filter.py
@@ -3,7 +3,6 @@
 # Importing python3 from local, just use "python3 <binary>" if is not the same location
 
 # Imports
-import sys
 import argparse
 import os
 import cv2
@@ -15,152 +14,162 @@ from filter_class import Filter
 initial_img = None
 first_finded_img = None
 first_finded_img_bool = False
-particle_nb = 30
+particle_nb = 40
 particle_size = [30, 30]
 
 
 # Function declarations
 def hsv_filter_detection(img):
-	img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	lower_hsv = np.array([0, 35, 190])
-	upper_hsv = np.array([179, 255, 255])
-	return cv2.inRange(img_hsv, lower_hsv, upper_hsv)
+    lower_hsv = np.array([0, 35, 190])
+    upper_hsv = np.array([179, 255, 255])
+    return cv2.inRange(img_hsv, lower_hsv, upper_hsv)
 
 
 def draw_squares(img, particles_class, mode):
 
-	if mode == "perturbation":
-		particles_array = particles_class.get_particles_perturbation()
-		for particle in particles_array:
-			x, y, w, h = particle.get_coords()
+    if mode == "perturbation":
+        particles_array = particles_class.get_particles_perturbation()
+        for particle in particles_array:
+            x, y, w, h = particle.get_coords()
 
-			cv2.rectangle(img, (x, y), (x + w, y + h), (234, 160, 86), 2)
-	else:
-		particles_array = particles_class.get_particles()
-		for particle in particles_array:
-			x, y, w, h = particle.get_coords()
+            cv2.rectangle(img, (x, y), (x + w, y + h), (234, 160, 86), 2)
+    elif mode == "prediction":
+        particles_array = particles_class.get_particles_prediction()
+        for particle in particles_array:
+            x, y, w, h = particle.get_coords()
 
-			if particle.get_score() > 0 and mode == "selection":
-				cv2.rectangle(img, (x, y), (x + w, y + h), (234, 160, 86), 2)
-			if particle.get_chosen() and mode == "chosen":
-				cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-			if mode == "all":
-				cv2.rectangle(img, (x, y), (x + w, y + h), (178, 178, 178), 2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (51, 133, 255), 2)
+    else:
+        particles_array = particles_class.get_particles()
+        for particle in particles_array:
+            x, y, w, h = particle.get_coords()
+
+            if particle.get_score() > 0 and mode == "selection":
+                cv2.rectangle(img, (x, y), (x + w, y + h), (102, 204, 0), 2)
+            if particle.get_chosen() and mode == "chosen":
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            if mode == "all":
+                cv2.rectangle(img, (x, y), (x + w, y + h), (178, 178, 178), 2)
 
 
 def get_img_sequence(dir_path):
-	if dir_path[-1] != "/":
-		dir_path += "/"
+    if dir_path[-1] != "/":
+        dir_path += "/"
 
-	imgs_path = [name for name in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, name))]
-	imgs_path = sorted(imgs_path, key=lambda x: int(os.path.splitext(x)[0]))
+    imgs_path = [name for name in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, name))]
+    imgs_path = sorted(imgs_path, key=lambda x: int(os.path.splitext(x)[0]))
 
-	imgs_path = [dir_path + i for i in imgs_path]
+    imgs_path = [dir_path + i for i in imgs_path]
 
-	return imgs_path
+    return imgs_path
 
 
 def first_frame_initialisation(img_shape):
-	global particle_nb
-	global particle_size
+    global particle_nb
+    global particle_size
 
-	return Filter(particle_nb, particle_size, img_shape[1], img_shape[0])
+    return Filter(particle_nb, particle_size, img_shape[1], img_shape[0])
 
 
 def draw_title(img, text):
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	org = (10, 30)
-	font_scale = 1
-	color = (255, 0, 0)
-	thickness = 2
-	cv2.putText(img, text, org, font, font_scale, color, thickness, cv2.LINE_AA)
-	return
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    org = (10, 30)
+    font_scale = 1
+    color = (255, 0, 0)
+    thickness = 2
+    cv2.putText(img, text, org, font, font_scale, color, thickness, cv2.LINE_AA)
+    return
 
 
 def draw_all_images(filter_class, original_img, mask, i):
-	global initial_img
-	global first_finded_img
-	global first_finded_img_bool
+    global initial_img
+    global first_finded_img
+    global first_finded_img_bool
 
-	black_img = np.zeros((original_img.shape[0], original_img.shape[1], 3), np.uint8)
+    black_img = np.zeros((original_img.shape[0], original_img.shape[1], 3), np.uint8)
 
-	mask_rgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
-	prediction_img = mask_rgb.copy()
-	selection_img = mask_rgb.copy()
-	chosen_img = mask_rgb.copy()
-	perturbation_img = mask_rgb.copy()
+    mask_rgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+    all_squares_img = mask_rgb.copy()
+    selection_img = mask_rgb.copy()
+    chosen_img = mask_rgb.copy()
+    perturbation_img = mask_rgb.copy()
+    prediction_img = mask_rgb.copy()
 
-	draw_squares(prediction_img, filter_class, "all")
-	draw_squares(selection_img, filter_class, "selection")
-	draw_squares(chosen_img, filter_class, "chosen")
-	draw_squares(perturbation_img, filter_class, "perturbation")
+    draw_squares(all_squares_img, filter_class, "all")
+    draw_squares(selection_img, filter_class, "selection")
+    draw_squares(chosen_img, filter_class, "chosen")
+    draw_squares(perturbation_img, filter_class, "perturbation")
+    draw_squares(prediction_img, filter_class, "prediction")
 
-	if i == 0:
-		initial_img = prediction_img.copy()
-		draw_title(initial_img, "Initialisation")
+    if i == 0:
+        initial_img = all_squares_img.copy()
+        draw_title(initial_img, "Initialisation")
 
-	if first_finded_img_bool is False:
-		if filter_class.get_last_chosen_particle() is not None:
-			first_finded_img = prediction_img.copy()
-			first_finded_img_bool = True
-		else:
-			first_finded_img = black_img.copy()
-		draw_title(first_finded_img, "First finded")
+    if first_finded_img_bool is False:
+        if filter_class.get_chosen_particles() is not []:
+            first_finded_img = all_squares_img.copy()
+            first_finded_img_bool = True
+        else:
+            first_finded_img = black_img.copy()
+        draw_title(first_finded_img, "First finded")
 
-	draw_title(original_img, "Original")
-	draw_title(mask_rgb, "Mask")
-	draw_title(prediction_img, "Prediction")
-	draw_title(selection_img, "Selection")
-	draw_title(chosen_img, "Estimation")
-	draw_title(perturbation_img, "Perturbation")
+    draw_title(original_img, "Original")
+    draw_title(mask_rgb, "Mask")
+    draw_title(all_squares_img, "Poblacion")
+    draw_title(selection_img, "Selection")
+    draw_title(chosen_img, "Estimation")
+    draw_title(perturbation_img, "Perturbation")
+    draw_title(prediction_img, "Prediction")
 
-	tmp1 = cv2.hconcat([original_img, mask_rgb, initial_img])
-	tmp2 = cv2.hconcat([first_finded_img, prediction_img, selection_img])
-	tmp3 = cv2.hconcat([chosen_img, perturbation_img, black_img])
-	final_img = cv2.vconcat([tmp1, tmp2, tmp3])
+    tmp1 = cv2.hconcat([original_img, mask_rgb, initial_img])
+    tmp2 = cv2.hconcat([first_finded_img, all_squares_img, chosen_img])
+    tmp3 = cv2.hconcat([selection_img, perturbation_img, prediction_img])
+    final_img = cv2.vconcat([tmp1, tmp2, tmp3])
 
-	cv2.imshow('hsv', final_img)
-	cv2.waitKey()
+    cv2.imshow('hsv', final_img)
+    cv2.waitKey()
 
 
 def get_arguments():
-	ap = argparse.ArgumentParser()
+    ap = argparse.ArgumentParser()
 
-	ap.add_argument("-d", "--directory", required=True, help="Path of the data directory")
-	ap.add_argument("-p", "--perturbation_differential", required=False, help="Apply perturbation with a differential method", action='store_true')
+    ap.add_argument("-d", "--directory", required=True, help="Path of the data directory")
+    ap.add_argument("-p", "--perturbation_differential", required=False, help="Apply perturbation with a differential method", action='store_true')
 
-	args = vars(ap.parse_args())
-	dir_path = args['directory']
-	perturbation_differential = args['perturbation_differential']
+    args = vars(ap.parse_args())
+    dir_path = args['directory']
+    perturbation_differential = args['perturbation_differential']
 
-	if not os.path.isdir(dir_path):
-		print("[ERROR] Data directory not found")
-		exit(84)
+    if not os.path.isdir(dir_path):
+        print("[ERROR] Data directory not found")
+        exit(84)
 
-	return dir_path, perturbation_differential
+    return dir_path, perturbation_differential
 
 
 def main():
-	dir_path, perturbation_differential = get_arguments()
-	imgs_path = get_img_sequence(dir_path)
+    dir_path, perturbation_differential = get_arguments()
+    imgs_path = get_img_sequence(dir_path)
 
-	for i, img_path in enumerate(imgs_path):
-		img = cv2.imread(img_path)
-		mask = hsv_filter_detection(img)
+    for i, img_path in enumerate(imgs_path):
+        img = cv2.imread(img_path)
+        mask = hsv_filter_detection(img)
 
-		if i == 0:
-			filter_class = first_frame_initialisation(img.shape)
+        if i == 0:
+            filter_class = first_frame_initialisation(img.shape)
 
-		filter_class.intialise_random()
+        filter_class.intialise_random()
 
-		filter_class.calculate_score(mask)
-		filter_class.choose_particle()
-		filter_class.perturbation(perturbation_differential)
+        filter_class.calculate_score(mask)
+        filter_class.choose_particle()
+        filter_class.perturbation(perturbation_differential)
+        filter_class.prediction()
 
-		draw_all_images(filter_class, img, mask, i)
+        draw_all_images(filter_class, img, mask, i)
 
 
 # Main body
 if __name__ == '__main__':
-	main()
+    main()
